789ad31a285a7cede0cc9f6b7c9a9a3bb8dbc664
Felipe A. Felix <felipe.felix@xpi.com.br>
2021-01-30 20:40:26 -0300

    feat: integrate Zabbix and Service now
    In order to register a new maintenance period for
    GMUDs, we've implemented a new feature to integrate
    Service Now Change workflow with Zabbix

--- Hacka13HubConnector/__init__.py
+++ Hacka13HubConnector/__init__.py
@@ -1,28 +1,37 @@
 import logging
 import os
+import datetime as dt
+from pytz import timezone
 import azure.functions as func
 from azure.data.tables import TableServiceClient
+
+try:
+    from pyzabbix.api import ZabbixAPI
+except ImportError:
+    logging.error("missing py-zabbix dependency")
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     req_body = req.get_json()
     change_number = req_body.get("change_number")
+    short_description = req_body.get("short_description")
     configuration_item =  req_body.get("configuration_item")
     planned_start_date = req_body.get("planned_start_date")
-    planned_end_date = req_body.get("planned_start_date")
+    planned_end_date = req_body.get("planned_end_date")
     
     logging.info(f'Processando solicitação de registro de janela de manutenção no Zabbix para mudança: {change_number}')
 
-    # TODO: Call Zabbix and register new maintenance alert window for requested configuration item
-
-    register_maintenance_alert_window_request(change_number, configuration_item, planned_start_date, planned_end_date)
+    start_date = dt.datetime.strptime(planned_start_date, '%Y-%m-%d %H:%M:%S')
+    end_date = dt.datetime.strptime(planned_end_date, '%Y-%m-%d %H:%M:%S')
+    register_maintenance_period(change_number, host_id=configuration_item, description=short_description, start_date=start_date, end_date=end_date)
+    register_maintenance_period_log(change_number, configuration_item, planned_start_date, planned_end_date)
 
     return func.HttpResponse(
-            f'Recebi as seguintes informacoes: {change_number} - {configuration_item} - {planned_start_date} - {planned_end_date}',
-            status_code=200
+            f'Janela de Manutenção cadastrada para mudança: {change_number}',
+            status_code=201
     )
 
-def register_maintenance_alert_window_request(change_number, configuration_item, planned_start_date, planned_end_date):
+def register_maintenance_period_log(change_number, configuration_item, planned_start_date, planned_end_date):
     connection_string = os.environ["AzureWebJobsStorage"]
     table_name = os.environ["EntityName"]
 
@@ -38,3 +47,40 @@ def register_maintenance_alert_window_request(change_number, configuration_item,
     }
 
     table_client.create_entity(entity)
+
+def get_host_id(zapi, host_name=None):
+    result = zapi.do_request('host.get', {"search": {"name": host_name}})['result']
+    if result and len(result) > 0:
+        return result[0]['hostid']
+
+def create_maintenance(zapi, host_id=None, name=None, description=None, start=None, end=None):
+    param = {
+        "groupids": [],
+        "hostids": [host_id],
+        "name": name,
+        "maintenance_type": "0",
+        "description": description,
+        "active_since": str(start),
+        "active_till": str(end),
+        "timeperiods": [
+            {
+                "timeperiod_type": 0,
+                "start_date": str(start),
+                "period": end-start
+            }]
+    }
+    return zapi.do_request('maintenance.create', params=param)
+
+# Press the green button in the gutter to run the script.
+def register_maintenance_period(change_number, host_id, description, start_date, end_date):
+    server_url = os.environ["ZabbixServerUrl"]
+    login_user = os.environ["ZabbixLogin"]
+    login_password = os.environ["ZabbixPassword"]
+    # Create ZabbixAPI class instance
+    with ZabbixAPI(url=server_url, user=login_user, password=login_password) as zapi:
+        host_id = get_host_id(zapi, host_id)
+        sao_paulo_timezone = timezone('America/Sao_Paulo')
+        start = int(start_date.replace(tzinfo=sao_paulo_timezone).timestamp())
+        end = int(end_date.replace(tzinfo=sao_paulo_timezone).timestamp())
+        maintenance = create_maintenance(zapi, host_id=host_id, name=change_number, description=description, start=start, end=end)
+        logging.info(maintenance)
